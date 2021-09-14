dfcbe2ea1fac47172e2a9f249bbae5ead716d154
Victoria <Sharkiiie@users.noreply.github.com>
2021-09-14 05:30:14 -0500

    Update items.js

--- src/assets/items.js
+++ src/assets/items.js
@@ -8,47 +8,47 @@ import youtube from '../assets/images (3).jpeg'
 import discord from '../assets/dipscord.png'
 
 const items = [{
-    "title": "Website",
-    "subtitle": "I'll make an updated webisite soon™",
-    "image": website,
-    "link": "#"
+    "title": "Discord",
+    "subtitle": "@Sharkie#6969 | Feel free to add me! | (Tap to open my profile)",
+    "image": discord,
+    "link": "https://discord.com/users/85809275888209920"
 },
-//{
-//    "title": "GitHub",
-//    "subtitle": "@devgossips | 🏠 of my open-source projects",
-//    "image": github,
-//    "link": "https://github.com/devgossips"
-//},
 {
     "title": "Instagram",
     "subtitle": "@forearms_international | Forearms. All day, every day.",
     "image": instagram,
     "link": "https://instagram.com/forearms_international"
 },
+{
+    "title": "Website",
+    "subtitle": "I'll make an updated webisite soon™",
+    "image": website,
+    "link": "#"
+},
+{
+    "title": "YouTube",
+    "subtitle": "BasedShark | Really garbage YT vids and bad gaming clips.",
+    "image": youtube,
+    "link": "https://www.youtube.com/channel/UCuKAOU_gRsTfLXYE7td5zlg"
+}]
+//{
+//    "title": "GitHub",
+//    "subtitle": "@devgossips | 🏠 of my open-source projects",
+//    "image": github,
+//    "link": "https://github.com/devgossips"
+//},
 //{
 //    "title": "Apps",
 //    "subtitle": "Hub of my awesome🔥 Apps",
 //    "image": appstore,
 //    "link": "#"
 //},
-{
-    "title": "YouTube",
-    "subtitle": "BasedShark | Really garbage YT vids and bad gaming clips.",
-    "image": youtube,
-    "link": "https://www.youtube.com/channel/UCuKAOU_gRsTfLXYE7td5zlg"
-},
-
 //{
 //    "title": "Dribbble",
 //    "subtitle": "@virtuonic | Hub to the Shots of my 😎 apps ",
 //    "image": dribbble,
 //    "link": "https://dribbble.com/virtuonic"
 //},
-{
-    "title": "Discord",
-    "subtitle": "@Sharkie#6969 | Feel free to add me!",
-    "image": discord,
-    "link": "https://discord.com/users/85809275888209920"
-}]
+
 
 export default items
